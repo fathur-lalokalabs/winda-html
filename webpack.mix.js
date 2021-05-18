f88f441ab5362b3b25c5a929e1b2242e4f4ba3d8
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2021-05-18 10:38:07 +0800

    feat: bootstrap components page

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,13 +2,11 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'public/js');
 
-// windmill custom js
+// admin js
 
 mix.combine([
     'src/js/init-alpine.js',
-    // 'src/js/charts-bars.js',
-    // 'src/js/charts-lines.js',
-    // 'src/js/charts-pie.js',
+    'src/js/init-bootstrap.js',
 ], 'public/js/admin.js');
 
 // tailwindCSS
